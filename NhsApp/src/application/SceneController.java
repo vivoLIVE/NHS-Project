d00f36d488ed93b45b15e4d5c81731d88d6eaea0
J K <vivolive1@gmail.com>
2023-11-07 16:29:02 +0000

    File SceneController.java was added
    File application.css was added
    
    File Main.java was added
    
    File Main.fxml was added
    
    File ceneController.fxml was added

---
+++ NhsApp/src/application/SceneController.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneController {
+	private Stage stage;
+	private Scene scene;
+	
+	
+	public void switchToMain(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToGPsSearchScene(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("GPsSearchScene.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
